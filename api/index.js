d37e41672f24b718990b59758dd3b7c40aa9ac11
sahchandan781 <sahchandan781@gmail.com>
2024-02-22 22:45:35 +0530

    added

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+import userRouter from './routes/user.route.js';
+
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log("connected to MongoDB");
+}).catch((err)=> {
+ console.log(err);
+}
+)
+;
+const app = express();
+
+app.listen(3000, () =>{
+    console.log('server is running on port 3000');
+});
+
+app.use('/api/user', userRouter);
+
+
+
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import { FaSearch } from "react-icons/fa"
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
