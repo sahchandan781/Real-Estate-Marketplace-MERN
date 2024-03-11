d03365db72443494ddaafa995b7d2a9e55f4364a
sahchandan781 <sahchandan781@gmail.com>
2024-03-11 19:04:04 +0530

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,7 +11,7 @@ import {
   signOutUserStart
 } from '../redux/user/userSlice.js';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 export default function Profile() {
@@ -22,6 +22,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   //console.log(formData);
 
   const dispatch = useDispatch();
@@ -125,7 +127,20 @@ export default function Profile() {
 
     }
   }
-
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -159,18 +174,48 @@ export default function Profile() {
           p-3 uppercase hover:opacity-95
           disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
 
-          <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center
           hover:opacity-95' to={"/create-listing"}>Create Listing
-          </Link>
+        </Link>
       </form>
 
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
 
+
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'user is updated successfully!' : ''}</p>
+      <button className='text-green-700 w-full' onClick={handleShowListings}>Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
+
+      {userListings && userListings.length > 0 &&
+
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl 
+          font-semibold'>Your Listings</h1>
+          {userListings.map((listing) =>
+            <div key={listing._id} className='border gap-4 rounded-lg p-3 flex justify-between 
+              items-center'>
+              <Link to={`/listing.imageUrls[0]`}>
+                <img src={listing.imageUrls[0]}
+                  className='h-16 w-16 object-contain ' alt='listing cover' />
+
+              </Link>
+              <Link className='flex-1 text-slate-700 font-semibold 
+             hover:underline truncate gap-4' to={`/listing/${listing._id}`}>
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </div>
+            </div>
+          )}
+        </div>}
+        
     </div>
   )
 }
